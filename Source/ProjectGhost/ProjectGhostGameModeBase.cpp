e8a1f515bc426c7270f3404f79d472df71df142b
Kristen Skogholt Haave <kristen.s.haave@outlook.com>
2021-06-11 11:53:33 +0200

    resolved gamelift implementation
    Corrected up the previous contains some wrong variables.

--- Source/ProjectGhost/ProjectGhostGameModeBase.cpp
+++ Source/ProjectGhost/ProjectGhostGameModeBase.cpp
@@ -14,82 +14,84 @@ void AProjectGhostGameModeBase::BeginPlay()
 
     if (InitSDKOutcome.IsSuccess())
     {
-        auto OnStartGameSession = [](Aws::GameLift::Server::Model::GameSession GameSession GameSessionObj, void* params)
+        auto OnStartGameSession = [](Aws::GameLift::Server::Model::GameSession GameSessionObj, void* Params)
         {
             FStartGameSessionState* State = (FStartGameSessionState*)Params;
 
             State->Status = Aws::GameLift::Server::ActivateGameSession().IsSuccess();
         };
-    }
 
-    auto onUpdateGameSession = [](AWs::GameLift::Server::Model::UpdateGameSession UpdateGameSessionObj, void* Params)
-    {
-        FStartGameSessionState* State = (FUpdateGameSessionState*)Params;
-    };
 
-    auto OnProcessTerminate = [](void* Params)
-    {
-        FProcessTerminateState* State = (FProcessTerminateState*)Params;
-        auto GetTerminationTimeOutcome = Aws::GameLift::Server::GetTerminationTime();
-        if (GetTerminationTimeOutcome.IsSuccess())
+        auto OnUpdateGameSession = [](Aws::GameLift::Server::Model::UpdateGameSession UpdateGameSessionObj, void* Params)
         {
-            State->TerminationTime = GetTerminationTimeOutcome.GetResult();
-        }
+            FUpdateGameSessionState* State = (FUpdateGameSessionState*)Params;
+        };
 
-        auto ProcessEndingOutcome = Aws::GameLift::Server::ProcessEnding();
-
-        if (ProcessEndingOutcome.IsSuccess())
+        auto OnProcessTerminate = [](void* Params)
         {
+            FTerminateProcessState* State = (FTerminateProcessState*)Params;
+            auto GetTerminationTimeOutcome = Aws::GameLift::Server::GetTerminationTime();
+            if (GetTerminationTimeOutcome.IsSuccess())
+            {
+                State->TerminationTime = GetTerminationTimeOutcome.GetResult();
+            }
+
+            auto ProcessEndingOutcome = Aws::GameLift::Server::ProcessEnding();
+
+            if (ProcessEndingOutcome.IsSuccess())
+            {
+                State->Status = true;
+                FGenericPlatformMisc::RequestExit(false);
+            }
+        };
+
+        auto OnHealthCheck = [](void* Params)
+        {
+            FHealthCheckState* State = (FHealthCheckState*)Params;
             State->Status = true;
-            FGenericPlatformMisc::RequestExit(false);
-        }
-    };
 
-    auto OnHealthCheck = [](void* Params)
-    {
-        FHealthCheckState* State = (FHealthCheckState*)Params;
-        State->Status = true;
-
-        return State->Status;
-    };
+            return State->Status;
+        };
 
 
-    TArray<FString> CommandLineTokens;
-    TArray<FString> CommandLineSwitches;
-    int Port = FURL::UrlConfig.Default;
+        TArray<FString> CommandLineTokens;
+        TArray<FString> CommandLineSwitches;
+        int Port = FURL::UrlConfig.DefaultPort;
 
-    FCommandLine::Parse(FCommandLine::Get, CommandLineTokens, CommandLineSwitches);
+        FCommandLine::Parse(FCommandLine::Get(), CommandLineTokens, CommandLineSwitches);
 
-    for (FString str : CommandLineSwitches)
-    {
-        FString Key;
-        FString Value;
-
-        if (str.Split("=", &Key, &Value))
+        for (FString str : CommandLineSwitches)
         {
-            if (Key.Equals("port"))
-                Port = FCString::Atoi(*Value);
+            FString Key;
+            FString Value;
+
+            if (str.Split("=", &Key, &Value))
+            {
+                if (Key.Equals("port"))
+                    Port = FCString::Atoi(*Value);
+            }
         }
+
+        const char* LogFile = "aLogFile.txt";
+        const char** LogFiles = &LogFile;
+        auto LogParams = new Aws::GameLift::Server::LogParameters(LogFiles, 1);
+
+        auto Params = new Aws::GameLift::Server::ProcessParameters(
+            OnStartGameSession,
+            &StartGameSessionState,
+            OnUpdateGameSession,
+            &UpdateGameSessionState,
+            OnProcessTerminate,
+            &TerminateProcessState,
+            OnHealthCheck,
+            &HealthCheckState,
+            Port,
+            *LogParams
+        );
+
+        auto ProcessReadyOutcome = Aws::GameLift::Server::ProcessReady(*Params);
+
     }
-
-    const char* LogFile = "aLogFile.txt";
-    const char** LogFiles = &LogFile;
-    auto LogParams = new Aws::GameLift::Server::LogParameters(LogFiles, 1);
-
-    auto Params = new Aws::GameLift::Server::ProcessParameters(
-        OnStartGameSession,
-        &StartGameSessionState,
-        OnUpdateGameSession,
-        &UpdateGameSessionState,
-        OnProcessTerminate,
-        &ProcessTerminateState,
-        OnHealthCheck,
-        &HealthCheckState,
-        Port,
-        *LogParams
-    );
-
-    auto ProcessReadyOutcome = Aws::GameLift::Server::ProcessReady(*Params);
 
 
 #endif
